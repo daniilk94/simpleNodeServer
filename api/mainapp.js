9cd3d0422e831d8893251dabe7b28ae91615e357
Daniil Malinen <daniil.malinen@edu.karelia.fi>
2025-11-12 14:04:13 +0200

    Added swagger.yaml

--- api/mainapp.js
+++ api/mainapp.js
@@ -2,10 +2,23 @@ import express from "express";
 import indexPage from "./index.js";
 import dataPage from "./data.js";
 import homePage from "./homepage.js";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load("./swagger.yaml");
+const swaggerOptions = {};
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.use(
+  "/doc",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerOptions)
+);
 
 app.use("/", homePage);
 app.use("/index", indexPage);
